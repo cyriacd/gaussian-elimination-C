15e9c1d8249c113fc962208bdc69e0dd5f4daa89
Cyiac Domini <cyriacd@outlook.com>
2015-09-24 10:41:39 -0400

    more functionality

--- lab1.c
+++ lab1.c
@@ -7,29 +7,59 @@
 #include <stdlib.h>
 #include <math.h>
 
-void printMatrix(){
-  /*add print fucntion*/
+void printmatrix(float *m, int n){
+  int r=0, c=0;
+  printf("\n");
+  for(r=0;r<n;r++){
+    for(c=0;c<n;c++){
+      printf("[%f] ",*((float *)m + r*n+c));
+    }
+    printf("\n");
+  }
 }
 
-void multiply(){
-  /*generic multiply function*/
+void solveMatrix(float *m, int n){
+  float* id= (float *)malloc(n*n*sizeof(int));
+  float* e1= (float *)malloc(n*n*sizeof(int));
+  float* e2= (float *)malloc(n*n*sizeof(int));
+  int i=0,c=0,r=0;
+  for (i=0;i<n;i++){
+    *((float *)id + i*n+i) = 1.0;
+  }
+  e1 = id;
+  e2=id;
+  printf("id:");
+  printmatrix(id,n);
+  for (c=0;c<n;c++){
+    *((float *)e1 + c*n+c) = 1/(*((float *)m + c*n+c));
+    for(r=c+1;r<n;r++){
+      *((float *)e2 + r*n+c) = -1*(*((float *)m + r*n+c));
+      printf("e2:");
+      printmatrix(e2,n);
+      *((float *)e2 + r*n+c) = 0;
+    }
+    printf("e2:");
+    printmatrix(e2,n);
+    printf("e1:");
+    printmatrix(e1,n);
+    *((float *)e1 + c*n+c) = 1;
+  }
+  printf("e1:");
+  printmatrix(e1,n);
+
 }
 
 int main ()
 {
   /*Set up counter variables */
-  int r = 0, c = 0, n = 3;
-  float matrix[3][3];
+  int r = 0, c = 0, n = 4;
+  float matrix[4][4];
   for(r=0;r<n;r++){
     for(c=0;c<n;c++){
       printf("Enter %i, %i: ", r,c );
       scanf("%f",((float *)matrix + r*n+c));
     }
   }
-  for(r=0;r<n;r++){
-    for(c=0;c<n;c++){
-      printf("[%f] ", matrix[r][c] );
-    }
-    printf("\n");
-  }
+  printmatrix(&matrix[0][0],n);
+  solveMatrix(&matrix[0][0],n);
 }
